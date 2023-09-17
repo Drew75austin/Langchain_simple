d7953e496279aea214e4d6be1c1437e8d1e50c6a
andre <andre@DESKTOP-6VRPES4>
2023-09-17 16:20:01 -0500

    updates

--- langChain.py
+++ langChain.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns   
 import seaborn.objects as so
+import plotly.express as px
 
 Military_Data = pd.read_csv("military.csv") 
 
@@ -13,5 +14,17 @@ st.write(sns.scatterplot(x="Military_Spending_Billion_USD", y="GDP_Trillion_USD"
 st.pyplot(fig)
 
 
+# Sample data
+data = pd.DataFrame({
+    'x': [1, 2, 3, 4, 5],
+    'y': [10, 15, 13, 17, 20],
+    'category': ['A', 'B', 'A', 'C', 'B']
+})
+
+# Create a scatter plot using Plotly Express
+fig = px.scatter(data, x='x', y='y', color='category', title='Scatter Plot with Categories')
+
+# Display the Plotly scatter plot in the Streamlit app
+st.plotly_chart(fig)
 
 
