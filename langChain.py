0249d283ac2c21816043eb6c1eb0387fe3b33363
andre <andre@DESKTOP-6VRPES4>
2023-09-17 15:46:33 -0500

    updates

--- langChain.py
+++ langChain.py
@@ -12,16 +12,12 @@ import seaborn.objects as so
 # from langchain.chains import LLMChain
 
 
-
-key = os.environ.get('OPENAI_API_KEY')
-
 # Load the data into a data frame
 Military_Data = pd.read_csv("military.csv") 
 
 fig, ax = plt.subplots(figsize=(20,10))
 st.write(sns.scatterplot(x="Military_Spending_Billion_USD", y="GDP_Trillion_USD", size="Population_Millions",
-            sizes=(40, 400), alpha=.5, palette="muted",
-            height=6, data=Military_Data))
+            sizes=(40, 400), alpha=.5, palette="muted", data=Military_Data))
 
 
 st.pyplot(fig)
