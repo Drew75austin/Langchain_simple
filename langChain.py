3cbd3e2367d5330a9a0420acb2629704a1a82a87
andre <andre@DESKTOP-6VRPES4>
2023-09-17 16:23:24 -0500

    update

--- langChain.py
+++ langChain.py
@@ -6,25 +6,24 @@ import seaborn as sns
 import seaborn.objects as so
 import plotly.express as px
 
-Military_Data = pd.read_csv("military.csv") 
+data = pd.read_csv("military.csv") 
 
 fig, ax = plt.subplots(figsize=(20,10))
 st.write(sns.scatterplot(x="Military_Spending_Billion_USD", y="GDP_Trillion_USD", size="Population_Millions",
-            sizes=(40, 400), alpha=.5, palette="muted", data=Military_Data))
+            sizes=(40, 400), alpha=.5, palette="muted", data=data))
 st.pyplot(fig)
 
-
-# Sample data
-data = pd.DataFrame({
-    'x': [1, 2, 3, 4, 5],
-    'y': [10, 15, 13, 17, 20],
-    'category': ['A', 'B', 'A', 'C', 'B']
-})
-
 # Create a scatter plot using Plotly Express
-fig = px.scatter(data, x='x', y='y', color='category', title='Scatter Plot with Categories')
+fig = px.scatter(
+    data,
+    x='Military_Spending_Billion_USD',
+    y='GDP_Trillion_USD',
+    size='Population_Millions',
+    title='Military Spending vs. GDP with Population Size'
+)
 
-# Display the Plotly scatter plot in the Streamlit app
+# Display the Plotly scatter plot in the Streamlit app directly
 st.plotly_chart(fig)
+
 
 
